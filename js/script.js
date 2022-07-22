504c19633d64f86bde7b6630a703906259ab30a1
DeaDRomeO <barkovskiis@mail.ru>
2022-07-21 18:05:47 -0700

    add view works 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-let a = 'hello world';
+let divClient = document.querySelector("#myclient");
+
+let massClient = [];
+let t = new XMLHttpRequest();
+t.open("GET", "client_list.json", true);
+t.responseType = "json";
+t.send();
+t.onload = function () {
+  massClient = t.response;
+};
+
+setTimeout(function () {
+  for (let oneclient of massClient) {
+    let div = document.createElement("div");
+    div.innerHTML = `<h5>${oneclient.url}</h5><div>${oneclient.description}</div>`;
+    divClient.appendChild(div);
+    console.log(oneclient);
+  }
+}, 1000);
